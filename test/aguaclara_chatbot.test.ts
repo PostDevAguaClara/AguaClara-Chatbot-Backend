133f37d2538511c4ff6f6f7a33a48cc57439545a
Patrick <pje42@cornell.edu>
2026-06-01 18:39:35 -0400

    Initial commit

---
+++ test/aguaclara_chatbot.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AguaclaraChatbot from '../lib/aguaclara_chatbot-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aguaclara_chatbot-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AguaclaraChatbot.AguaclaraChatbotStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
