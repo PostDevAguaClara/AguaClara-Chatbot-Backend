b3f34c598757af3fdf7d8d901419aef67c3b2fab
Patrick <pje42@cornell.edu>
2026-07-28 18:31:50 -0400

    init

---
+++ constructs/knowledge-base.ts
@@ -0,0 +1,112 @@
+//knowledge-base.ts
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3vectors from 'aws-cdk-lib/aws-s3vectors';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as bedrock from 'aws-cdk-lib/aws-bedrock';
+
+import { Construct } from 'constructs';
+
+const TITAN_EMBEDDING_DIMENSION = 1024;
+
+export class KnowledgeBase extends Construct {
+  public readonly knowledgeBaseId: string;
+  public readonly knowledgeBaseArn: string;
+  public readonly dataSourceId: string;
+  public readonly documentsBucket: s3.Bucket;
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+    const region = cdk.Stack.of(this).region;
+    const embeddingModelArn = 
+        `arn:aws:bedrock:${region}::foundation-model/amazon.titan-embed-text-v2:0`;
+
+    // S3
+    const documentsBucket = new s3.Bucket(this, "ChatbotDocuments", {
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+        autoDeleteObjects: true,
+    });
+
+    // S3 Vector
+    const vectorBucket = new s3vectors.CfnVectorBucket(this, 'VectorBucket', {})
+    const vectorIndex = new s3vectors.CfnIndex(this, 'VectorIndex', {
+        vectorBucketArn: vectorBucket.attrVectorBucketArn,
+        dataType: 'float32',
+        dimension: TITAN_EMBEDDING_DIMENSION,
+        distanceMetric: 'cosine',
+        metadataConfiguration: {
+            'nonFilterableMetadataKeys': [
+                'AMAZON_BEDROCK_TEXT',
+                'AMAZON_BEDROCK_METADATA'
+            ]
+        }
+    });
+    vectorIndex.addDependency(vectorBucket);
+    
+    // IAM role
+    const kbRole = new iam.Role(this, 'kbRole', {
+        assumedBy: new iam.ServicePrincipal("bedrock.amazonaws.com")
+    });
+    kbRole.addToPolicy(
+        new iam.PolicyStatement({
+            actions: ["s3:GetObject", "s3:ListBucket"],
+            resources: [documentsBucket.bucketArn, `${documentsBucket.bucketArn}/*`]
+        })
+    );
+    kbRole.addToPolicy(
+        new iam.PolicyStatement({
+            actions: ["bedrock:InvokeModel"],
+            resources: [embeddingModelArn]
+        })
+    );
+    kbRole.addToPolicy(
+        new iam.PolicyStatement({
+            actions: [
+                "s3vectors:GetVectors",
+                "s3vectors:QueryVectors",
+                "s3vectors:PutVectors",
+                "s3vectors:DeleteVectors",
+                "s3vectors:GetIndex",
+            ],
+            resources: [vectorIndex.attrIndexArn]
+        })
+    );
+
+    // Knowledgebase
+    const kb = new bedrock.CfnKnowledgeBase(this, 'ChatbotKB', {
+        name: 'chatbot-knowledge-base',
+        roleArn: kbRole.roleArn,
+        knowledgeBaseConfiguration: {
+            type: 'VECTOR',
+            vectorKnowledgeBaseConfiguration: {
+                embeddingModelArn: embeddingModelArn
+            }
+        },
+        storageConfiguration: {
+            type: 'S3_VECTORS',
+            s3VectorsConfiguration: {
+                vectorBucketArn: vectorBucket.attrVectorBucketArn,
+                indexArn: vectorIndex.attrIndexArn,
+            }
+        }
+    });
+    kb.node.addDependency(vectorIndex);
+    kb.node.addDependency(kbRole);
+
+    // Data Source
+    const dataSource = new bedrock.CfnDataSource(this, 'ChatbotDataSource', {
+        name: 'chatbot-data-source',
+        description: 'The custome data source for the AguaClara chatbot KB',
+        knowledgeBaseId: kb.attrKnowledgeBaseId,
+        dataSourceConfiguration: {
+            type: 'CUSTOM',
+        }
+    })
+
+    // Exposed values
+    this.knowledgeBaseId = kb.attrKnowledgeBaseId;
+    this.knowledgeBaseArn = kb.attrKnowledgeBaseArn;
+    this.dataSourceId = dataSource.attrDataSourceId;
+    this.documentsBucket = documentsBucket;
+  }
+}
--- lib/aguaclara_chatbot-stack.ts
+++ lib/aguaclara_chatbot-stack.ts
@@ -1,16 +1,115 @@
-import * as cdk from 'aws-cdk-lib/core';
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as s3n from 'aws-cdk-lib/aws-lambda-event-sources';
+import * as iam from 'aws-cdk-lib/aws-iam';
+
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as events from 'aws-cdk-lib/aws-events';
+import * as targets from 'aws-cdk-lib/aws-events-targets';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
+
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { KnowledgeBase } from '../constructs/knowledge-base';
 
 export class AguaclaraChatbotStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Document Knowledgebase
+    const kb = new KnowledgeBase(this, 'Knowledgebase');
+    const documentsBucket = kb.documentsBucket;
+    
+    // Lambdas
+    const syncLambda = new lambda.Function(this, 'GoogleDriveSyncLambda', {
+        runtime: lambda.Runtime.NODEJS_22_X,
+        handler: 'googleSync.handler',
+        code: lambda.Code.fromAsset('lambda'),
+        environment: {
+          INPUT_BUCKET: documentsBucket.bucketName,
+          KNOWLEDGEBASE_ID: kb.knowledgeBaseId,
+          KNOWLEDGEBASE_SOURCE_ID: kb.dataSourceId,
+        },
+        timeout: cdk.Duration.minutes(10),
+        memorySize: 256,
+    });
+    documentsBucket.grantRead(syncLambda);
+    documentsBucket.grantWrite(syncLambda);
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AguaclaraChatbotQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const chatLambda = new lambda.Function(this, "ChatLambda", {
+      runtime: lambda.Runtime.NODEJS_22_X,
+      handler: "chat.handler",
+      code: lambda.Code.fromAsset("lambda"),
+      environment: {
+        KNOWLEDGEBASE_ID: kb.knowledgeBaseId,
+      },
+      timeout: cdk.Duration.seconds(30),
+      memorySize: 512,
+    });
+    chatLambda.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: [
+          "bedrock:Retrieve",
+          "bedrock:RetrieveAndGenerate",
+          "bedrock:InvokeModel",
+        ],
+        resources: ["*"],
+      })
+    );
+
+    // Secrets
+    const googleSecret = secretsmanager.Secret.fromSecretNameV2(
+      this,
+      'GoogleDriveSecret',
+      'demo-google-service-account'
+    );
+    googleSecret.grantRead(syncLambda);
+    syncLambda.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: [
+          "ssm:GetParameter",
+          "ssm:PutParameter"
+        ],
+        resources: [
+          `arn:aws:ssm:${this.region}:${this.account}:parameter/chatbot/demo-drive/*`
+        ]
+      })
+    );
+    syncLambda.addToRolePolicy(
+      new iam.PolicyStatement({
+        sid: "UpdateBedrockDocuments",
+        actions: [
+          "bedrock:IngestKnowledgeBaseDocuments",
+          "bedrock:DeleteKnowledgeBaseDocuments",
+          "bedrock:StartIngestionJob",
+          "bedrock:GetIngestionJob",
+          "bedrock:ListIngestionJobs",
+          "bedrock:GetKnowledgeBase",
+          "bedrock:GetDataSource",
+          "bedrock:GetKnowledgeBaseDocuments",
+        ],
+        resources: [
+          "*"
+        ]
+      })
+    );
+        
+    // API Gateways
+    const syncApi = new apigateway.LambdaRestApi(this, 'DriveSyncApi', {
+      handler: syncLambda,
+      proxy: true,
+    });
+    new cdk.CfnOutput(this, 'SyncApiUrl', {
+      value: syncApi.url,
+    });
+
+    const chatApi = new apigateway.LambdaRestApi(this, 'ChatApi', {
+      handler: chatLambda,
+      proxy: true,
+    });
+    new cdk.CfnOutput(this, 'ChatApiUrl', {
+      value: chatApi.url,
+    });
+
   }
 }
